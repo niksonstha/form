b5b00ffeb81b01024bfbc26533c68cef9483274b
niksonstha <sthanikson10@gmail.com>
2022-11-09 16:57:11 +0545

    first commit

---
+++ src/components/FormField.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import "./FormField.css";
+const Form = () => {
+  const [firstnameInput, setFirstnameInput] = useState("");
+  const [lastnameInput, setLastnameInput] = useState("");
+  const [firstnameHead, setFirstnameHead] = useState();
+  const [lastnameHead, setLastnameHead] = useState();
+
+  let inputChangeOne = (event) => {
+    setFirstnameInput(event.target.value);
+    console.log(event.target.value);
+  };
+  let inputChangeTwo = (event) => {
+    setLastnameInput(event.target.value);
+    console.log(event.target.value);
+  };
+  let onSubmit = () => {
+    setFirstnameHead(firstnameInput);
+    setLastnameHead(lastnameInput);
+    setFirstnameInput("");
+    setLastnameInput("");
+  };
+  return (
+    <>
+      <div className="form">
+        <h1>
+          Hello, {firstnameHead} {lastnameHead}
+        </h1>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          onChange={inputChangeOne}
+          value={firstnameInput}
+        />
+        <input
+          type="text"
+          placeholder="Enter your name"
+          onChange={inputChangeTwo}
+          value={lastnameInput}
+        />
+
+        <button onClick={onSubmit}>Click me 👍</button>
+      </div>
+    </>
+  );
+};
+
+export default Form;
